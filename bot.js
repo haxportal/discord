3333a1a0836b39bfcb553bdab8a0e3a240840e65
haxportal <40777491+haxportal@users.noreply.github.com>
2018-07-06 22:22:48 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
+client.login(process.env.NDYzNDE2NDA4Mjg2NjkxMzI4.DiFS4Q.sF7Yd_OIgUqzvGNklgO84newP5w);//where BOT_TOKEN is the token of our bot 
